ad9751552cb31c9787b82d6379a3ced1029fa9c8
Ibrahim Ali Hamim <ibrahimalihamim@gmail.com>
2023-09-23 16:10:01 +0600

    class-7(blog)

---
+++ JavaScript/array.js
@@ -0,0 +1,15 @@
+let father = {
+    name : "Abdur Rauf",
+    phone : 987654654,
+    Blood : "O-",
+    profession : "Business",
+}
+
+let Mother = {
+    name : "Hasina Akter",
+    phone : 93845723,
+    Blood : "B+",
+    profession : "House Wife",
+}
+
+let fatherkeys = 
