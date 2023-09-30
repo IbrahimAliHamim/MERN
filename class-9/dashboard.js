a6216cf3ce94fa56bafda0b66517b19df7970c04
Ibrahim Ali Hamim <ibrahimalihamim@gmail.com>
2023-09-30 16:29:46 +0600

    Dashboard_updated

--- class-9/dashboard.js
+++ class-9/dashboard.js
@@ -1,40 +1,81 @@
-// Initialize variables
-let depositTotal = 0;
-let withdrawnAmount = 0;
 
-// Function to update the display of the total amount
-function updateTotal() {
-    const totalAmount = depositTotal - withdrawnAmount;
-    document.getElementById('totalAmount').textContent = totalAmount.toFixed(2);
-}
+const depositButton = document.getElementById('diposit');
+depositButton.addEventListener('click', function () {
+    // Get deposit input value
+    const depositInput = document.getElementById('dipositValue');
+    const depositAmount = parseFloat(depositInput.value);
 
-// Function to handle deposit
-document.getElementById('depositBtn').addEventListener('click', function () {
-    const depositValue = parseFloat(document.getElementById('depositValue').value);
-
-    if (!isNaN(depositValue) && depositValue > 0) {
-        depositTotal += depositValue;
-        updateTotal();
-        document.getElementById('depositTotal').textContent = depositTotal.toFixed(2);
+    // Check if the input is a valid number
+    if (isNaN(depositAmount) || depositAmount <= 0) {
+        alert("Please enter a valid positive number for deposit.");
+        depositInput.value = '';
+        return;
     }
 
-    // Clear the input field after deposit
-    document.getElementById('depositValue').value = '';
-});
+    // Get current deposit total
+    const currentDeposit = document.getElementById('DipositTotal');
+    const currentDepositAmount = parseFloat(currentDeposit.innerText);
 
-// Function to handle withdrawal
-document.getElementById('withdrawBtn').addEventListener('click', function () {
-    const withdrawValue = parseFloat(document.getElementById('withdrawValue').value);
+    // Calculate new deposit total
+    const newDepositTotal = currentDepositAmount + depositAmount;
 
-    if (!isNaN(withdrawValue) && withdrawValue > 0 && (depositTotal - withdrawnAmount) >= withdrawValue) {
-        withdrawnAmount += withdrawValue;
-        updateTotal();
-        document.getElementById('withdrawnAmount').textContent = withdrawnAmount.toFixed(2);
+    // Update the deposit total
+    currentDeposit.innerText = newDepositTotal.toFixed(2);
+
+    // Update the total balance
+    const totalBalance = document.getElementById('totalValue');
+    const totalBalanceAmount = parseFloat(totalBalance.innerText);
+    const newTotalBalance = totalBalanceAmount + depositAmount;
+    totalBalance.innerText = newTotalBalance.toFixed(2);
+
+    // Check for alert conditions (every 100 or thousand)
+    if (newTotalBalance % 100 === 0 || newTotalBalance % 1000 === 0) {
+        alert(`You've reached ${newTotalBalance} in total deposits!`);
     }
 
-    // Clear the input field after withdrawal
-    document.getElementById('withdrawValue').value = '';
+    // Clear the input field
+    depositInput.value = '';
 });
 
-// Initial update of the total amount
-updateTotal();
+const withdrawButton = document.getElementById('withdraw');
+withdrawButton.addEventListener('click', function () {
+    // Get withdrawal input value
+    const withdrawInput = document.getElementById('WithdrawValue');
+    const withdrawAmount = parseFloat(withdrawInput.value);
+
+    // Check if the input is a valid number
+    if (isNaN(withdrawAmount) || withdrawAmount <= 0) {
+        alert("Please enter a valid positive number for withdrawal.");
+        withdrawInput.value = '';
+        return;
+    }
+
+    // Get current withdrawal total
+    const currentWithdrawal = document.getElementById('withdrawMain');
+    const currentWithdrawalAmount = parseFloat(currentWithdrawal.innerText);
+
+    // Calculate new withdrawal total
+    const newWithdrawalTotal = currentWithdrawalAmount + withdrawAmount;
+
+    // Update the withdrawal total
+    currentWithdrawal.innerText = newWithdrawalTotal.toFixed(2);
+
+    // Update the total balance
+    const totalBalance = document.getElementById('totalValue');
+    const totalBalanceAmount = parseFloat(totalBalance.innerText);
+    
+    // Ensure the total doesn't go below 0
+    const newTotalBalance = Math.max(totalBalanceAmount - withdrawAmount, 0);
+    
+    // Update the total balance
+    totalBalance.innerText = newTotalBalance.toFixed(2);
+
+    // Check for alert conditions (total less than 5)
+    if (newTotalBalance < 5) {
+        alert("Warning: Total balance is less than 5!");
+    }
+
+    // Clear the input field
+    withdrawInput.value = '';
+});
+
